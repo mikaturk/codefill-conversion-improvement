c927e650e912cbeeec3167e6e8ad15117f79d234
Mika Turk <git@mikaturk.nl>
2022-05-19 13:32:44 +0200

    Speed improvements to convert

--- notebooks/convert/convert_benchmark.py
+++ notebooks/convert/convert_benchmark.py
@@ -2,16 +2,24 @@
 # %%
 
 import os
+import shutil
+import tempfile
 import tokenize
 import dis
 import sys
 import re
 import keyword
+import numpy as np
 import pandas as pd
 import ast
 import torch
 import signal
 from functools import wraps
+from io import BytesIO, StringIO
+
+debug_filenames = True
+save_stdout = sys.stdout
+
 
 def multireplace(string, replacements, ignore_case=False):
     """
@@ -24,6 +32,9 @@ def multireplace(string, replacements, ignore_case=False):
     # If case insensitive, we need to normalize the old string so that later a replacement
     # can be found. For instance with {"HEY": "lol"} we should match and find a replacement for "hey",
     # "HEY", "hEy", etc.
+    if replacements == {}:
+        return string
+    
     if ignore_case:
         def normalize_old(s):
             return s.lower()
@@ -359,6 +370,206 @@ def convert_new(file, output_file):
     with open(output_file,'w') as f:
         f.write(code_converted)
 
+# %%
+
+# https://stackoverflow.com/questions/1883980/find-the-nth-occurrence-of-substring-in-a-string
+# def find_nth(haystack, needle, n, start=0):
+# def find_nth(haystack, needle, n):
+# #     start = haystack.find(needle, start)
+#     start = haystack.find(needle)
+#     while start >= 0 and n > 1:
+#         start = haystack.find(needle, start+len(needle))
+#         n -= 1
+#     return start
+
+# def before(token):
+#     return token.split('(')[2].split(')')[0]
+
+# def after(token):
+#     start = find_nth(token, "(", 2) + 1
+#     end = find_nth(token, ')', 1)
+#     return token[start:end]
+
+
+# %%
+
+# find_nth("hello(world((test)))", "(", 2)
+
+# %%
+def convert_new_v2(file, output_file):
+    if debug_filenames: print("starting "+output_file, file=save_stdout)
+    with open (file, "r") as f:
+        text = f.read()  
+
+    replacements = {}
+    for node in ast.iter_child_nodes(ast.parse(text)):
+        if isinstance(node, ast.ImportFrom):
+            replacements.update({node.module: 'MODULE'})
+        if isinstance(node, ast.Import) or isinstance(node, ast.ImportFrom):
+            for i, v in enumerate(node.names):
+                if(node.names[i].asname):
+                    replacements.update({node.names[i].name: 'LIB'})                
+                    replacements.update({node.names[i].asname: 'ALIAS'})
+                else:
+                    replacements.update({node.names[i].name: 'LIBRARY'})
+
+
+    # reomve * from the dictionary (handle from module import * statement)
+    replacements.pop('*', None)
+    # print('List of modules and libraries to replace:\n', replacements)
+
+    # with open('med.py','w') as f:
+    #     f.write(multireplace(text, replacements, ignore_case = True))
+    med = multireplace(text, replacements, ignore_case = True)
+
+    # file = 'med.py'
+    # with open(file,'rb') as f:
+    #     tokens = list(tokenize.tokenize(f.readline))
+    tokens = tokenize.tokenize(BytesIO(med.encode('utf-8')).readline)
+
+        
+    ### extract important data from the output of tokenize package
+
+    last_line = 0
+    last_pos = 0
+    tokss = []
+    for token in tokens:
+        
+        tok_org = token.string
+        tok_text = token.string    
+        # tok_type = str(token).split('(')[2].split(')')[0]
+        tok_type = tokenize.tok_name[token.type]
+
+        # convert keywords to upper
+        if keyword.iskeyword(tok_text):
+            tok_type = str.upper(tok_text)
+        
+        #extract operations
+        # if tok_type == 'OP':
+        #     tok_type = tok_text
+
+
+        # getting rid of comments and empty lines
+        if tok_type in ['NL','NEWLINE','COMMENT']:
+            continue
+        
+        #retrieve the position
+        tok_line = token.start[0]
+        
+        if last_line == tok_line:
+            last_pos +=  1
+        else:
+            last_pos = 1
+        tok_pos = last_pos
+        last_line = tok_line
+        
+        tokss.append({'type':tok_type,
+                            'original':tok_org,
+                            'text':tok_text,
+                            'line':tok_line,
+                            'pos':tok_pos})
+
+    toks = pd.DataFrame(tokss)
+
+    # remove encoding lines and end of file
+    toks.line = toks.line.astype('int')
+    toks.pos = toks.pos.astype('int')
+    toks = toks.loc[~((toks.type == 'ENCODING') | (toks.type == 'ENDMARKER'))]
+    toks['doc'] = (toks.text.str.contains('"""') | toks.text.str.contains("'''"))
+    toks = toks.loc[~(toks.doc)].drop(['doc'],axis=1)
+
+    # toks.head(20)
+
+    indent = 0
+    last_line = 0
+
+    tokss = [] # PERF
+
+    for row in toks.itertuples():
+        if row.type == "INDENT":
+            indent +=1
+            continue
+        if row.type == "DEDENT":
+            indent -=1
+            continue
+        if row.line != last_line:
+            last_line = row.line
+            tokss.append({'type':'\n'+indent*'\t',
+                                'text':'\n'+indent*'\t',
+                                'line':row.line,
+                                'pos':row.pos-1})
+    
+    toks = pd.concat([toks, pd.DataFrame(tokss)])
+
+    toks = toks.loc[~((toks.type=='INDENT') | (toks.type=='DEDENT'))]
+    toks = toks.sort_values(['line','pos']).reset_index(drop=True)
+
+
+    # drop the first row (empty line)
+    toks.drop(toks.index[:1], inplace=True)
+
+    # toks.head(20)
+
+    # with open(file,'r') as f:
+    #     src = f.read()
+
+    src = text
+    stdout_backup = sys.stdout
+    dis_result = StringIO()
+    sys.stdout = dis_result
+    dis.dis(src)
+    sys.stdout = stdout_backup
+
+    # with open(tmp_dir + '/dis.txt','r') as f:
+    #     lines = f.readlines()
+    lines = dis_result.getvalue().split('\n')
+
+    # find global variables
+    glbls = [].copy()    
+    for l in lines:
+        clean = l.replace('>>',' ').strip().split()
+        if len(clean):
+            try:
+                int(clean[1])
+                line = int(clean[0])
+            except:
+                clean = [str(line)]+clean
+            if 'LOAD_GLOBAL' in clean:
+                # print('found a global!')
+                glbls.append((int(clean[0]),clean[-1].replace('(','').replace(')','')))
+
+    # adjust_lines = [True] * toks.shape[0]
+    for l,n in glbls:
+        # toks.loc[(toks.line==l) & (toks.text==n),'type'] = 'GLOBAL_VARIABLE'
+        line_eq = toks.loc[toks.line==l]
+        line_eq.loc[line_eq.text==n, 'type'] = 'GLOBAL_VARIABLE'
+        
+    # toks.loc[adjust_lines, 'type'] = 'GLOBAL_VARIABLE'
+
+    # toks .head(10) 
+
+    # text_imports = ' '.join(list(toks.text)).replace('\n ','\n').replace(' \n','\n').replace('\t ','\t').replace(' . ','.').replace(' (','(')
+    # text_imports = multireplace(text_imports, replacements, ignore_case = True)
+
+    # with open('normalized_textual_file.py','w') as f:
+    #     f.write(text_imports)
+
+    # toks.type = toks.apply(lambda x: x['text'] if x['text'] in ['LIBRARY','LIB','ALIAS','MODULE'] else x['type'], axis = 1)
+    toks.loc[toks['text'].isin(['LIBRARY','LIB','ALIAS','MODULE']), 'type'] = toks['text']
+
+    code_converted = ' '.join(list(toks.type)).replace('\n ','\n').replace(' \n','\n').replace('\t ','\t').replace(' . ','.').replace(' (','(')
+
+    final_replacements = {'GLOBAL_VARIABLE(':'FUNCTION_CALL(',                      
+    #                       'NAME.NAME':'NAME',
+                          'NAME(':'FUNCTION_CALL(',
+                          'NAME':'LOCAL_VARIABLE'}
+
+    code_converted = multireplace(code_converted, final_replacements, ignore_case = False)
+
+    with open(output_file,'w') as f:
+        f.write(code_converted)
+    if debug_filenames: print("finished "+output_file, file=save_stdout)
+
 # %% 
 from pathlib import Path
 import glob
@@ -373,10 +584,26 @@ def convert_n(n_paths, conv):
             converted_paths.append(converted_path)
         except:
             pass
+# %%
+os.chdir('/home/mturk/rp/codefill/notebooks')
+
+%load_ext line_profiler
 
 # %%
-%load_ext line_profiler
-# %lprun -f convert convert("sample_data/data/raw_to_mat.py", "sample_data/converted/raw_to_mat.txt")
-%lprun -f convert convert_n(1, convert)
+# %lprun -f convert_new convert_new("slowfiles/test_randomstate.py", "slowfiles/test_randomstate.txt")
+# %lprun -f convert_new_v2 convert_new_v2("slowfiles/test_randomstate.py", "slowfiles/test_randomstate.txt")
+# %lprun -f convert_new convert_new("slowfiles/drive_v2.py", "slowfiles/drive_v2.txt")
+# %lprun -f convert_new_v2 convert_new_v2("slowfiles/drive_v2.py", "slowfiles/drive_v2.txt")
+%lprun -f convert_new_v2 convert_new_v2("slowfiles/rpdb2.py", "slowfiles/rpdb2.txt")
+# %lprun -f convert_new_v2 convert_new_v2("slowfiles/sp_applications.py", "slowfiles/sp_applications.txt")
+# %lprun -f convert_new_v2 convert_new_v2("slowfiles/data_se.py", "slowfiles/data_se.txt")
+# %lprun -f convert_new_v2 convert_new_v2("slowfiles/145.py", "slowfiles/145.txt")
+# %lprun -f convert convert_n(1, convert)
 
+# %%
+%lprun -f convert convert("slowfiles/145.py", "slowfiles/145.txt")
+
+# %%
+
+convert_new_v2("slowfiles/145.py", "slowfiles/145.txt")
 # %%
